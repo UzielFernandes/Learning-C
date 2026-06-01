b2eb31d955f306b7d802920c9ccd3021678511b1
Uziel Fernandes <uzielfern07@gmail.com>
2026-06-02 00:09:16 +0530

    Added Variables file
    Added Variables file which covers core variable teachings

---
+++ Variables.c
@@ -0,0 +1,25 @@
+//Variables in C
+
+#include <stdio.h> 
+#include <stdbool.h> //exclusive library needed for boolean
+
+int main() { 
+    int a = 6; //for numbers
+    printf("Hello World! %d \n", a); // %d is formal specifier for int
+
+    char b = 'c'; //for characters
+    printf("Hello World %c \n", b); // %c is fs
+
+    char c[] = "Good Morning!"; //for statement/s via arrays
+    printf("Hello World, %s \n", c); // %s is fs
+
+    float d = 6.5; //for float/decimal values
+    printf("Hello World %f \n", d); // %f is fs, be default shows 6 values after decimal point
+    printf("Hello World %.2f \n", d); // any number before f specifies no of digits after decimal point
+    printf("Hello World %.10lf \n", d); // for more digits, %lf is fs
+
+    bool e = true; //for boolean
+    printf("Hello World %d \n", e); // %d again is the fs
+
+    return 0; 
+}
