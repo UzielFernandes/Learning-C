c41acdff3d61224f475112842b5c98a419ace23c
Uziel Fernandes <uzielfern07@gmail.com>
2026-06-01 23:50:08 +0530

    Added first file
    Added a fresh new file, the first file of this repo describing the structure of a basic C workspace

---
+++ First_File.c
@@ -0,0 +1,9 @@
+//C Sructure
+
+#include <stdio.h> //importing library containing all essential C function
+
+int main() { //the main function in which many a code is embedded
+    printf("Hello World!"); //print statement
+
+    return 0; //good practise
+}
