a51f091a2bb89625c849a0fbbfdba48511d11fce
Uziel Fernandes <uzielfern07@gmail.com>
2026-06-06 00:10:55 +0530

    whitespace update
    user input with whitespaces

--- User_Input.c
+++ User_Input.c
@@ -3,9 +3,10 @@
 int main() {
     char colour[20]; //declaring a variable with no value attached
 
-    printf("\nwhat clour is your bujatti? "); //demo question
-    scanf("%19s", &colour); /*format specifier in quotes and the name of the variable we declared
+    printf("\nwhat colour is your bujatti? "); //demo question
+    /*scanf("%19s", &colour); /*format specifier in quotes and the name of the variable we declared
                             & is not necessary for arrays like this example but very important for other formats */
 
+    fgets(colour, 19, stdin);
     printf("oh so your bugatti is %s?", colour); //working as normal!
 }
