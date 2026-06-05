ae0d07774715cc211320a2f2305e0d3fdb9007aa
Uziel Fernandes <uzielfern07@gmail.com>
2026-06-05 23:45:45 +0530

    added new file
    added new file for demonstration of user input

---
+++ User_Input.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main() {
+    int colour[20]; //declaring a variable with no value attached
+
+    printf("\nwhat clour is your bujatti? "); //demo question
+    scanf("%19s", &colour); /*format specifier in quotes and the name of the variable we declared
+                            & is not necessary for arrays like this example but very important for other formats */
+
+    printf("oh so your bugatti is %s?", colour); //working as normal!
+}
