c07649cf34b788248abb235a92334a4b5bd8e29a
Uziel Fernandes <uzielfern07@gmail.com>
2026-06-15 23:16:32 +0530

    Added Bubble Sort

---
+++ BubbleSort.c
@@ -0,0 +1,34 @@
+//BUBBLE SORT!
+
+#include <stdio.h>
+
+void sorter(int arr[], int sizearr) {
+    int temp;
+    for(int i = 0; i < sizearr-1; i++) {
+
+        for (int j = 0; j < sizearr-i-1; j++) {
+
+            if(arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                
+            }
+        }
+    }
+}
+
+void printer(int arr[], int sizearr) {
+    for (int k = 0; k < sizearr; k++) {
+        printf("%d ", arr[k]);
+    }
+}
+
+int main() {
+    int arr[] = {9,6,7,3,5,3};
+    int sizearr = (sizeof(arr)/sizeof(arr[0]));
+
+    sorter(arr, sizearr);
+    printer(arr,sizearr);
+}
