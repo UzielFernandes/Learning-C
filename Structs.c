0923ffb40cd895f5e22ebe8e7abddd8181dc61c6
Uziel Fernandes <uzielfern07@gmail.com>
2026-06-17 19:12:10 +0530

    Updated structs to include typedef

--- Structs.c
+++ Structs.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-struct attributes{
+struct attributes{ //structs
     char name[20];
     int age;
 };
 
+typedef struct{ //typedef + structs
+    char itemcode[20];
+    int price;
+}iteminfo;
+
 int main() {
+    //structs demo
     struct attributes Man1;
     struct attributes Woman1;
-    struct attributes Woman2 = {"Liz", 46};
+    struct attributes Woman2 = {"Liz", 46}; //also works
 
     strcpy(Man1.name, "John");
     Man1.age = 15;
@@ -18,7 +24,12 @@ int main() {
     Woman1.age = 17;
 
     printf("%d\n", Man1.age);
-    printf("%s", Woman1.name);
-    printf("%s, %d", Woman2.name, Woman2.age);
+    printf("%s\n", Woman1.name);
+    printf("%s, %d\n", Woman2.name, Woman2.age);
+
+    //typedef + structs demo
+    iteminfo item1 = {"abc123", 45};
+    iteminfo item2 = {"efg567", 22};
+    printf("%s, %d", item1.itemcode, item2.price);
 }
 
